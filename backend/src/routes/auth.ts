24da02bac63dd6d02be93f189bbfab6560e5ee89
kalavh <jefferson_1996Th@hotmail.com>
2020-09-10 22:44:28 -0300

    arrumando erro

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,6 @@
 const router =  require('express').Router()
 const Users = require('../Database/Models/User')
-const login = require('../Controllers/RP/register')
+const register = require('../Controllers/RP/register')
 
     router.post('/register',
         (req,res) => register(req,res) ) 
