a4b6d2fbc24f2da59b125f1812136240bf2bb559
kalavh <jefferson_1996Th@hotmail.com>
2020-09-12 16:41:52 -0300

    incio jwt

--- backend/src/Controllers/App/authController.ts
+++ backend/src/Controllers/App/authController.ts
@@ -1,4 +1,5 @@
 const User = require('../../Database/Models/User')   
+const jwt = require('jsonwebtoken')
    
 const userRegister = async (req,res) => {
         const { login , password } = req.body
@@ -12,6 +13,9 @@ const userLogin = async (req,res) => {
         res.send('Login page')
 }
 
+const createToken = (id) => { 
+        return jwt.sing({ id },'segredo hash' )
+}
 
                 
 module.exports = {userRegister,userLogin}        
