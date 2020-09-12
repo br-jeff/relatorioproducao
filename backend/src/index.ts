e6adce522613ffd96c016b9c8fb43ed55b49094e
kalavh <jefferson_1996Th@hotmail.com>
2020-09-11 21:56:07 -0300

    rota de usuario mudada

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const auth = require('./routes/auth')
-const routes = require('./Routes/auth')
+const authRoutes = require('./Routes/auth')
 const port = 3000
 
 app.use(express.json())
 
-app.use(routes)
+app.use('/app/user',authRoutes)
 
 app.listen(port, 
     () => console.log(`Servidor na porta ${port}`))
