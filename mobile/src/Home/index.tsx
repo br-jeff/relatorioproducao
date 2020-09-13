b173cdefa47386037f757b974831bafe1bbee39e
kalavh <jefferson_1996Th@hotmail.com>
2020-09-12 23:12:54 -0300

    Criacao de duas paginas basicas

---
+++ mobile/src/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react-native'
+import {View,Text} from' react-native'
+
+const Home: React.FC  = () => {
+    return(
+        <View>
+         <Text> Home page</Text>
+        </View> 
+    )
+}
+export default Home
---
+++ mobile/src/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react-native'
+import {View,Text} from' react-native'
+
+const Login: React.FC  = () => {
+    return(
+        <View>
+         <Text> login page</Text>
+        </View> 
+    )
+}
+export default Login
