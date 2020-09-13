527c3ae1ec0c50a591109fbe310b1c4b827d9939
kalavh <jefferson_1996Th@hotmail.com>
2020-09-13 15:36:45 -0300

    agora ele mantem o usuario logado

--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -1,4 +1,5 @@
-import React, {createContext,useState} from 'react'
+import React, {createContext,useState,useEffect} from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 import * as auth from '../services/auth'
 
 interface AuthContextData{
@@ -11,11 +12,24 @@ const AuthContext = createContext<AuthContextData>( {} as AuthContextData);
 
 export const AuthProvider: React.FC = ( {children} ) => {
     const [ user, setUser ] = useState<object | null>(null)
+    useEffect(()=> {
+        async function loadStorageData() {
+         const storagedUser = await AsyncStorage.getItem('@rp:user')
+         const storagedToken = await AsyncStorage.getItem('@rp:token')   
 
+         if(storagedUser &&  storagedToken ) {
+             setUser(JSON.parse(storagedUser))
+         }
+        }
+        loadStorageData() 
+     },[])
     async function singIn() {
        const response = await auth.singIn()
 
        setUser(response.user)
+       console.log(JSON.stringify(response.user) +  response.token)
+       await AsyncStorage.setItem('@rp:user',JSON.stringify(response.user))
+       await AsyncStorage.setItem('@rp:token',response.token)
     }
 
     return ( 
