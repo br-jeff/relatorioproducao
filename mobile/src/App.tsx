c8acd6ef44c9b47a92b2b258dc15a2be2cb82212
kalavh <jefferson_1996Th@hotmail.com>
2020-09-13 13:11:34 -0300

    expotando authcontext como component para o app

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,16 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import AuthContext from './contexts/auth'
+import {AuthProvider} from './contexts/auth'
 
 import Routes from './routes'
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-        <AuthContext.Provider value={{logado: true}}>
+      <AuthProvider>
          <Routes/>
-       </AuthContext.Provider>
+      </AuthProvider>
     </NavigationContainer>
   )
 }
--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -1,5 +1,12 @@
-import  {createContext} from 'react'
+import React, {createContext} from 'react'
 
 const AuthContext = createContext({logado: true});
+
+export const AuthProvider: React.FC = ( {children} ) => (
+    <AuthContext.Provider value={{logado: false}}>
+        {children}
+   </AuthContext.Provider>  
+);
+
 
 export default AuthContext;
