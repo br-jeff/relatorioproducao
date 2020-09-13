7539ea969a4234d843dfe1b3269dbde9f4936eb0
kalavh <jefferson_1996Th@hotmail.com>
2020-09-13 14:41:14 -0300

    login funcionando com dados falsos

--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -1,23 +1,25 @@
-import React, {createContext} from 'react'
+import React, {createContext,useState} from 'react'
 import * as auth from '../services/auth'
 
 interface AuthContextData{
     logado: boolean;
-    user: object
+    user: object | null
     singIn(): Promise<void>
 }
 
 const AuthContext = createContext<AuthContextData>( {} as AuthContextData);
 
 export const AuthProvider: React.FC = ( {children} ) => {
-    
+    const [ user, setUser ] = useState<object | null>(null)
+
     async function singIn() {
        const response = await auth.singIn()
-       console.log(response)
+
+       setUser(response.user)
     }
 
     return ( 
-        <AuthContext.Provider value={{logado: false,user:{},singIn}}>
+        <AuthContext.Provider value={{logado: !!user,user,singIn}}>
             {children}
         </AuthContext.Provider>  
    )
