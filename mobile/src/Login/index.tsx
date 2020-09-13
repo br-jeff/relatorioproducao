d05013d81ca71805cb3239cbceefa064a3fd95ff
kalavh <jefferson_1996Th@hotmail.com>
2020-09-13 12:08:07 -0300

    Layouyt base para tela de login

--- mobile/src/Login/index.tsx
+++ mobile/src/Login/index.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import React from 'react';
+import { View,Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
 
-const Login: React.FC  = () => {
-    return(
-        <View>
-         <Text> login page</Text>
-        </View> 
-    )
+const Login: React.FC = () => {
+    return (
+     <View style={styles.container} >
+         <View style={styles.moldura} >
+            <TextInput style={styles.input}  placeholder=" Digite Seu usuario" />
+                <TextInput style={styles.input}  placeholder=" Digite Sua Senha" />
+           
+               <RectButton style={styles.loginButton}>
+                   <Text style={styles.loginText}> Login </Text>
+               </RectButton>
+            </View>
+        </View>
+      );
 }
-export default Login
+
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fafafa',
+    } ,
+    moldura: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 300,
+        height: 300,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        elevation: 3,
+    } ,
+    input:{
+        borderWidth: 0.3,
+        borderRadius: 4,
+        width: '90%',
+        marginTop: 18,
+
+    },  
+    loginText: {
+        fontSize: 17,
+        color: '#fff'
+    },
+    loginButton: {
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: '#29f',
+        margin: 30,
+        padding: 7,
+        height: 40,
+        width:'90%',
+        borderRadius: 20,
+
+    },
+
+})
+export default Login;
