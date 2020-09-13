5424210cc36ef6771f3d127cb22797e93e084a8f
kalavh <jefferson_1996Th@hotmail.com>
2020-09-13 14:35:26 -0300

    interface do services onde recebe token e user

--- mobile/src/services/auth.ts
+++ mobile/src/services/auth.ts
@@ -1,10 +1,19 @@
-export function singIn() { 
+interface Response {
+    token: string;
+    user: {
+        login : string
+        nome: string
+    };
+}
+
+export function singIn(): Promise<Response> { 
     return new Promise( resolve=> { 
         setTimeout( ()=> { 
             resolve({
                 token: 'almsçsamlkq2poij213op34klm465lmk',
                 user: {
-                    login: 'jefferson'
+                    login: 'jefferson',
+                    nome: 'jeferson ferreira da silva'
                 }
             })
         },100)
